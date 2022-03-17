d0be74c0d8dab36985c51fae5a8cc659e0d6c672
Liam <mr.liumingjie@gmail.com>
2022-03-17 23:49:41 +0800

    update

---
+++ Math/Combi.cpp
@@ -0,0 +1,61 @@
+//求组合数的几种方法
+//不确定的时候都开 long long
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int mod = 1e9 + 7;
+//C(a, b) a上b下
+
+/*1. 依照定义   当a, b很小的时候（几十）*/
+int C(ll a, int b) // a上b下
+{   
+    if(a < b) return 0;
+    int up = 1, down = 1;
+    for (ll i = a; i > a - b; i -- ) up = i % mod * up % mod; //up *= i
+    for (int j = 1; j <= b; j ++) down = (ll)j * down % mod; // down *= j
+    return (ll)up * qpow(down, mod - 2) % mod; //   (up/down)
+}
+
+/*2. 递推 杨辉三角  a, b在2000这个数量级*/
+//O(N^2) 1e6~1e7
+void init()
+{
+    for (int i = 0; i < N; i ++)
+        for (int j = 0; j <= i; j ++)
+            if(!j) C[i][j] = 1;
+            else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod;
+}
+
+/*3. 预处理fac[], invfac[]*/
+//O(N) 1e6左右 看N大小
+int fac[N], invfac[N];
+void init()
+{
+    fac[0] = invfac[0] = 1;
+    for (int i = 1; i < N; i ++)
+    {
+        fac[i] = 1ll * fac[i - 1] * i % mod;
+        invfac[i] = 1ll * invfac[i - 1] * qpow(i, mod - 2) % mod;
+    }
+    //调用 1ll * fac[a] * invfac[b] % mod * invfac[a - b] % mod
+}
+
+/*4. lucas定理 当a, b的值特别大 如1e9以上...1e18等*/
+int C(int a, int b)
+{
+    int res = 1;
+    for (int i = 1, j = a; i <= b; i ++, j --)
+    {
+        res = (ll)res * j % p;
+        res = (ll)res * binpow(i, p - 2) % p;
+    }
+    return res;
+}
+
+ll lucas(ll a, ll b)
+{//p为质数
+    if(a < p && b < p) return C(a, b);
+    return (ll)C(a % p, b % p) * lucas(a / p, b / p) % p;
+}
+
---
+++ Math/PIE.cpp
@@ -0,0 +1,58 @@
+//cf451E acwing214
+/**
+ * S = {n1*a1, n2*a2 ... nk*ak}
+ * 从中取出r个元素产生不同多重集的数量
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int mod = 1e9 + 7, N = 20;
+ll A[N], n, m;
+
+int qpow(int a, int b)
+{
+    int res = 1;
+    while(b) {
+        if(b&1) res = (ll)res*a % mod;
+        b >>= 1;
+        a = (ll)a * a % mod;
+    }
+    return res;
+}
+
+int C(ll a, int b) // a上b下
+{   
+    if(a < b) return 0;
+    int up = 1, down = 1;
+    for (ll i = a; i > a - b; i -- ) up = i % mod * up % mod;
+    for (int j = 1; j <= b; j ++) down = (ll)j * down % mod;
+    return (ll)up * qpow(down, mod - 2) % mod;
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; i ++)  cin >> A[i];
+
+    int res = 0;
+    for (int i = 0; i < 1 << n; i ++)
+    {
+        ll a = m + n - 1, b = n - 1;
+        int sign = 1;
+        for (int j = 0; j < n; j ++)
+        {
+            if(i >> j & 1)
+            {
+                sign *= -1;
+                a -= A[j] + 1;
+            }
+        }
+        res = (res + C(a, b) * sign) % mod;
+    }
+    cout << (res + mod) % mod;
+
+    return 0;
+}

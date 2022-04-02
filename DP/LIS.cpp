6238c1ce93ed2e59c943c9c6b64ccdc80c71331b
Liam <mr.liumingjie@gmail.com>
2022-04-02 21:28:07 +0800

    update

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -26,26 +26,88 @@ int main()
 }
 
 //2. 维护类似单调递增的队列
+//stl （若是要求最长下降序列，需要用lower_bound(q, q + pos + 1, a[i], greater<int>())）
+//不能同upper_bound()
+/*
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 100010;
+int a[N], d1[N], d2[N], n;
+int main() 
+{
+	while (cin >> a[++n])
+        ;
+    n--;                       
+    int len1 = 1, len2 = 1;		
+	d1[1] = a[1];		
+	d2[1] = a[1];
 
-int q[N];
+    for (int i = 2; i <= n; i++)
+    {		
+		if (d1[len1] >= a[i])
+            d1[++len1] = a[i]; 
+        else 
+            *upper_bound(d1 + 1, d1 + 1 + len1, a[i], greater<int>()) = a[i];
+		
+		if (d2[len2] < a[i]) d2[++len2] = a[i];		
+		else
+            *lower_bound(d2 + 1, d2 + 1 + len2, a[i]) = a[i];
+
+	}
+	cout << len1 << endl << len2;		
+	return 0;		
+}
+*/
+//一下求了最长上升子序列和最长下降子序列
+#include<iostream>
+#include<cstdio>
+using namespace std;
+const int N=100100;
+int n, len;
+int a[N], q1[N], q2[N];
 
 int main()
 {
-    cin >> n;
-    for (int i = 0; i < n; i ++) cin >> a[i];
+    while(scanf("%d",&a[++n]) == 1)
+        ;
+    n--;
+    q1[0] = a[1];
+    q2[0] = a[1];
 
-    int pos = 0;
-    q[0] = a[0];
+    int pos1 = 0, pos2 = 0;
+    for (int i = 2; i <= n; i++)
+    {	
+		if (q1[pos1] >= a[i]) q1[++pos1] = a[i];		
+		else 
+        {
+            int l = 0, r = pos1, mid;
+            while(l < r)
+            {
+                mid = (l + r) >> 1;
+                if(q1[mid] >= a[i]) l = mid + 1;
+                else r = mid;
+            }
+            q1[l] = a[i];
+        }
 
-    for (int i = 1; i < n; i++)
-    {
-        if(a[i] > q[pos])
-            q[++pos] = a[i];
-        else
-            *lower_bound(q, q + pos + 1, a[i]) = a[i];
+        if (q2[pos2] < a[i]) q2[++pos2] = a[i];	
+		else
+        {
+            int l = 0, r = pos2, mid;
+            while(l < r)
+            {
+                mid = l + r >> 1;
+                if(q2[mid] < a[i]) l = mid + 1;
+                else r = mid;
+            }
+            q2[l] = a[i];
+            
+        }
     }
 
-    cout << pos + 1;
-
+	cout << pos1 + 1 << endl << pos2 + 1;
+   
     return 0;
 }
+  
---
+++ DataStructure/persistentSegTree.cpp
@@ -0,0 +1,75 @@
+//luogu 3824 kth-number
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5 + 10, M = (N << 2) + 17 * N;
+
+struct node {
+    int l, r;
+    int cnt;
+} t[M];
+int idx, a[N];
+vector<int> num;
+int find(int x) { return lower_bound(num.begin(), num.end(), x) - num.begin(); }
+
+
+int insert(int now, int l, int r, int x) {
+    int p = ++ idx;
+    t[p] = t[now];
+    if (l == r) {
+        t[p].cnt ++ ;
+        return p;
+    }
+    int mid = l + r >> 1;
+    if(x <= mid) t[p].l = insert(t[now].l, l, mid, x);
+    else t[p].r = insert(t[now].r, mid + 1, r, x);
+    t[p].cnt = t[t[p].l].cnt + t[t[p].r].cnt;
+
+    return p;
+}
+
+int build(int l, int r) {
+    int p = ++ idx;
+    if (l == r) return p;
+    int mid = l + r >> 1;
+    t[p].l = build(l, mid), t[p].r = build(mid + 1, r);
+    return p;
+}
+
+int query(int x, int y, int l, int r, int k) {
+    if(l == r) return l;
+    int cnt = t[t[y].l].cnt - t[t[x].l].cnt;
+    int mid = l + r >> 1;
+    if(k <= cnt) return query(t[x].l, t[y].l, l, mid, k);
+    else return query(t[x].r, t[y].r, mid + 1, r, k - cnt);
+
+}
+
+
+int n, m, root[N];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i ++ ) {
+        scanf("%d", &a[i]);
+        num.push_back(a[i]);
+    }
+
+    sort(num.begin(), num.end());
+    num.erase(unique(num.begin(), num.end()), num.end());
+
+    root[0] = build(0, num.size() - 1);
+
+    for (int i = 1; i <= n; i ++ )
+        root[i] = insert(root[i - 1], 0, num.size() - 1, find(a[i]));
+
+    while (m -- )
+    {
+        int l, r, k;
+        scanf("%d%d%d", &l, &r, &k);
+        printf("%d\n", num[query(root[l - 1], root[r], 0, num.size() - 1, k)]);
+    }
+
+    return 0;
+}

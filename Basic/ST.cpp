ec4ca3d0a673f86718b3868cfa21237700c49598
Liam <mr.liumingjie@gmail.com>
2022-04-07 14:08:34 +0800

    add sparse table algo

---
+++ Basic/ST.cpp
@@ -0,0 +1,42 @@
+//ST表
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int f[N][21], n, m;
+int a[N];
+//f[i][j]表示区间[i, i + 2^j - 1]的最大值
+
+void init()
+{
+    for (int j = 0; j < 21; j ++)
+        for (int i = 1; i + (1 << j) - 1 <= n; i++)//区间长度是2^j所以要减一
+            if(!j)  f[i][j] = a[i];
+            else
+                f[i][j] = max(f[i][j - 1], f[i + (1 << j - 1)][j - 1]);
+}
+
+int query(int l, int r)
+{
+    int len = r - l + 1;
+    int k = __lg(len);
+    return max(f[l][k], f[r - (1 << k) + 1][k]);
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+
+    init();
+
+    while(m -- )
+    {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        printf("%d\n", query(l, r));
+    }
+
+    return 0; 
+}

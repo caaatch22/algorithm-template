89b7327d4ba75bb7d3d4dd99b240e41b968c3155
Liam <mr.liumingjie@gmail.com>
2022-04-10 13:38:44 +0800

    update dinic algo

--- Graph/BipartiteMatch2.cpp
+++ Graph/BipartiteMatch2.cpp
@@ -8,15 +8,11 @@ vector<int> G[N];
 int w[N], p[N];
 bool vis[N];
 
-bool match(int u)
-{
-    for(auto v:G[u])
-    {
-        if(!vis[v])
-        {
+bool match(int u) {
+    for(auto v:G[u]) {
+        if(!vis[v]) {
             vis[v] = true;
-            if(p[v] == 0 || match(p[v]))
-            {
+            if(p[v] == 0 || match(p[v])) {
                 p[v] = u;
                 return true;
             }
@@ -27,24 +23,19 @@ bool match(int u)
 
 int get_id(int x, int y) {return (x - 1)* n + y;}
 
-int main()
-{
+int main() {
     cin >> n >> t;
-    while(t -- )
-    {
+    while(t -- ) {
         int x, y;
         cin >> x >> y;
         w[get_id(x, y)] = 1;
     }
     
-    for(int i = 1; i <= n; i ++)
-    {
-        for(int j = 1; j <= n; j ++)
-        {
+    for(int i = 1; i <= n; i ++) {
+        for(int j = 1; j <= n; j ++) {
             int id = get_id(i, j);
             if(w[id]) continue;
-            for(int k = 0; k < 4; k ++)
-            {
+            for(int k = 0; k < 4; k ++) {
                 int x = i + dx[k], y = j + dy[k];
                 if(x < 1 || y < 1 || x > n || y > n) continue;
                 int id2 = get_id(x, y);
@@ -55,8 +46,7 @@ int main()
     }
     
     int res = 0;
-    for(int i = 1; i <= n * n; ++i)
-    {
+    for(int i = 1; i <= n * n; ++i) {
         memset(vis, 0, sizeof(vis));
         if(match(i)) res ++;
     }
@@ -64,3 +54,87 @@ int main()
     
     return 0;
 }
+
+/*dinic解二分图
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define pb push_back
+using namespace std;
+using ll = long long;
+const int N = 1e4 + 10;
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+int n, m, s, t, dep[N];
+struct node {int v, cap, rec;};
+vector<node> G[N];
+
+bool bfs() {
+    queue<int> q;
+    q.push(s);
+    memset(dep, -1, sizeof dep);
+    dep[s] = 0;
+    while (q.size()) {
+        int u = q.front(); q.pop();
+        for(auto [v, cap, rev] : G[u]) {
+            if(dep[v] == -1 && cap) {
+                dep[v] = dep[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+    return dep[t] != -1;
+}
+
+ll dfs(int u, ll lim) {
+    if(u == t || lim == 0) return lim;
+    ll tot_flow = 0;
+    for(auto& [v, cap, rev] : G[u]) {
+        if(dep[v] == dep[u] + 1 && cap > 0) {
+            ll d = dfs(v, min(lim, (ll)cap));
+            cap -= d;
+            G[v][rev].cap += d;
+            lim -= d;
+            tot_flow += d;
+            if(lim == 0)  return tot_flow;
+        } 
+    }
+    if(lim != 0) dep[u] = -1;
+    return tot_flow;
+}
+
+ll dinic() {
+    ll res = 0;
+    while(bfs())
+        res += dfs(s, inf);
+    return res;
+}
+
+void add(int u, int v, int cap) {
+    G[u].pb({v, cap, G[v].size()});
+    G[v].pb({u, 0, G[u].size() - 1});
+}
+
+int main() {
+    scanf("%d%d", &m, &n);
+    s = 0, t = n + 1;
+    
+    for (int i = 1; i <= m; i ++ ) add(s, i, 1);
+    for (int i = m + 1; i <= n; i ++ ) add(i, t, 1);
+
+    int a, b;
+    while (cin >> a >> b, a != -1) add(a, b, 1);
+
+    printf("%d\n", dinic());
+
+    for(int i = 1; i <= m; i ++) 
+        for(auto [v, cap, rev] : G[i])
+            if(cap == 0 && v >= m + 1)
+                printf("%d %d\n", i, v);
+
+    return 0;
+}
+*/
---
+++ Graph/Dinic.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define pb push_back
+using namespace std;
+using ll = long long;
+const int N = 1e4 + 10;
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+int n, m, s, t, dep[N];
+struct node {int v, cap, rec;};
+vector<node> G[N];
+
+bool bfs() {
+    queue<int> q;
+    q.push(s);
+    memset(dep, -1, sizeof dep);
+    dep[s] = 0;
+    while (q.size()) {
+        int u = q.front(); q.pop();
+        for(auto [v, cap, rev] : G[u]) 
+            if(dep[v] == -1 && cap) 
+                dep[v] = dep[u] + 1, q.push(v);
+    }
+    return dep[t] != -1;
+}
+
+ll dfs(int u, ll lim) {
+    if(u == t || lim == 0) return lim;
+    ll tot_flow = 0;
+    for(auto& [v, cap, rev] : G[u]) {
+        if(dep[v] == dep[u] + 1 && cap > 0) {
+            ll d = dfs(v, min(lim, (ll)cap));
+            cap -= d, G[v][rev].cap += d;
+            lim -= d, tot_flow += d;
+            if(lim == 0)  return tot_flow;
+        } 
+    }
+    if(lim != 0) dep[u] = -1;
+    return tot_flow;
+}
+
+ll dinic() {
+    ll max_flow = 0;
+    while(bfs())
+        max_flow += dfs(s, inf);
+    return max_flow;
+}
+
+int main() {
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    while(m --) {
+        int u, v, cap;  scanf("%d%d%d", &u, &v, &cap);
+        G[u].pb({v, cap, G[v].size()});
+        G[v].pb({u, 0, G[u].size() - 1});
+    }
+
+    printf("%lld\n", dinic());
+    return 0;
+}

bd7e429c606840c8017dfa57c65dbdd2079e3716
Liam <mr.liumingjie@gmail.com>
2024-04-20 22:30:59 +0800

    update

--- DP/expDP.cpp
+++ DP/expDP.cpp
@@ -1,41 +1,45 @@
-//acw217
+/**
+ * 题意：有向无环图，从1号点到n号点，每个点有若干个出边，
+ * 每条边有一个权值，求从1号点到n号点的期望距离。
+ */
 #include <bits/stdc++.h>
-#define pb push_back
-using namespace std;
 
-const int N = 1e5 + 10;
-struct node{int v, w;};
-vector<node> G[N];
-int n, m, oud[N];
-double f[N]; //f[i]:从i跳到n的期望长度
+struct node {
+  int v, w;
+};
 
-double dp(int u)
-{
-    if(f[u] >= 0) return f[u];
-    if(u == n)  return f[u] = 0;
+int main() {
+  ios::sync_with_stdio(false), cin.tie(0);
+  cin >> n >> m;
+  auto f = std::vector<double>(n);
+  auto oud = std::vector<int>(n);
+  auto G = std::vector<std::vector<node>>(n);
+  while (m--) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    u--, v--;
+    G[u].emplace_back(v, w);
+    oud[u]++;
+  }
+
+  std::fill(f.begin(), f.end(), -1);
+  std::function<double(int)> dp = [&](int u) -> double {
+    if (f[u] >= 0) return f[u];
+    if (u == n) return f[u] = 0;
 
     f[u] = 0;
-    for(auto nxt:G[u])
-    {
-        int v = nxt.v, w = nxt.w;
-        f[u] += (w + dp(v)) / oud[u];
+    for (auto [v, w] : G[u]) {
+      f[u] += (w + dp(v)) / oud[u];
     }
     return f[u];
-}
+  };
+  std::cout << std::fixed << std::setprecision(2) << dp(0) << '\n';
 
-int main()
-{
-    ios::sync_with_stdio(false), cin.tie(0);
-    cin >> n >> m;
-    while(m -- )
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        G[u].pb({v, w});
-        oud[u]++;
-    }
-    memset(f, -1, sizeof f);
-    printf("%.2lf\n", dp(1));
-
-    return 0;
+  return 0;
 }
+/**
+ * 若还有自环（但是无其他环）
+ * dp(u) = \frac{w_{uu}}{oud[u]} dp(u) + \sum_{v, v!=u}
+ * \frac{w_{uv}}{oud[u]}dp(v)
+ * 需要将dp(u)移项到左侧，然后化简
+ */
